7aa2600b9f7371bf19835400da075a0fd5b5b6ce
ThreeSnakes <dev.select995@gmail.com>
2023-12-29 10:27:00 +0900

    [ADD] Config feature
    - app 설정시 사용하는 Config Feature 추가.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log'],
+  });
+  const configService = app.get(ConfigService);
+  const PORT = configService.get('PORT');
+
+  await app.listen(PORT);
 }
 bootstrap();

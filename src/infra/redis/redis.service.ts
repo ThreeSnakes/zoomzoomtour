b9546eabe899ddc097713b8bd7cc6174fcefb435
ThreeSnakes <dev.select995@gmail.com>
2023-12-31 21:50:28 +0900

    [UPDATE] RedisService 정리
    - 사용하지 않는 메소드 삭제

--- src/infra/redis/redis.service.ts
+++ src/infra/redis/redis.service.ts
@@ -8,36 +8,15 @@ export class RedisService {
     private readonly _redisClient: Redis,
   ) {}
 
-  async hmset(key: string, value: Object) {
-    return this.redisClient.hmset(key, value);
-  }
-
-  async set(key: string, value: string): Promise<string> {
-    return this.redisClient.set(key, value);
-  }
-
-  async get(key: string): Promise<string | null> {
-    return this.redisClient.get(key);
+  async hmset(key: string, value: object) {
+    return this._redisClient.hmset(key, value);
   }
 
   async hgetall(key: string) {
-    return this.redisClient.hgetall(key);
+    return this._redisClient.hgetall(key);
   }
 
   async hincrby(key: string, field: string, increment: number) {
-    return this.redisClient.hincrby(key, field, increment);
-  }
-
-  async hset(key: string, field: string, value: string) {
-    return this.redisClient.hset(key, field, value);
-  }
-
-  async hget(key: string, field: string): Promise<string> {
-    console.log(key, field);
-    return this.redisClient.hget(key, field);
-  }
-
-  get redisClient() {
-    return this._redisClient;
+    return this._redisClient.hincrby(key, field, increment);
   }
 }

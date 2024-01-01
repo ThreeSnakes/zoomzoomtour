870eba43afd42ee5884b54de9e6f0f1381860abc
ThreeSnakes <dev.select995@gmail.com>
2024-01-01 14:51:03 +0900

    [CHORE] Tour 관련 DTO 문서 수정
    - swagger 문서 관련 수정

--- src/module/tour/dto/api/modifyTourHolidaysRequest.dto.ts
+++ src/module/tour/dto/api/modifyTourHolidaysRequest.dto.ts
@@ -5,15 +5,16 @@ import { DAY_OF_WEEK } from '../../domain/regularHoliday.domain';
 export class ModifyTourHolidaysRequestDto {
   @ApiProperty({
     description:
-      '해당 투어의 정기 휴일, SUN, MON, TUE, WED, THU, FRI, SAT 중 쉬는 날을 array에 넣어서 보낸다.',
-    example: ['MON', 'TUE'],
+      '해당 투어의 정기 휴일, SUN(0), MON(1), TUE(2), WED(3), THU(4), FRI(5), SAT(6) 중 쉬는 날을 array에 넣어서 보낸다.',
+    example: [0, 1],
   })
   @IsOptional()
   regularHoliday?: DAY_OF_WEEK[];
 
   @ApiProperty({
-    description: '해당 투어의 특정 휴일, MM-DD 형태로 array에 넣어서 보낸다.',
-    example: ['01-01', '03-01'],
+    description:
+      '해당 투어의 특정 휴일, YYYY-MM-DD 형태로 array에 넣어서 보낸다.',
+    example: ['2024-01-01', '2024-03-01'],
   })
   @IsOptional()
   holiday?: string[];
